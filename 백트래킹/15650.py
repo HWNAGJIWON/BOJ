e7ef472cff385d7f0359b9441d234a0ccab020a2
hwangjiwon <5513019@naver.com>
2021-11-19 23:01:43 +0900

    11.19

--- 백트래킹/15650.py
+++ 백트래킹/15650.py
@@ -1,3 +1,22 @@
+# n과 m(2)
+
 import sys
 input = sys.stdin.readline
 
+n, m = [int(x) for x in input().split()]
+
+s = []
+
+def backtracking():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    s.sort()
+    for i in range(1, n+1):
+        if i not in s and (len(s) == 0 or s[len(s)-1] < i):
+            s.append(i)
+            backtracking()
+            s.pop()
+
+
+backtracking()
